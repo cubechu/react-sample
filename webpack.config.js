ee8c55f9a6ef093031620217af95fe1b381c6706
cube_zhu <cube_zhu@kingdee.com>
2016-05-10 11:22:19 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ let ExtractTextPlugin = require('extract-text-webpack-plugin');
 let ProgressBarPlugin = require('progress-bar-webpack-plugin');
 
 let plugins = [
-    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),//通过一些计算方式减少chunk的大小
     new ProgressBarPlugin({
         format: '  build [:bar] :percent (:elapsed seconds)',
         clear: false
@@ -17,8 +17,8 @@ let plugins = [
     })
 ];
 plugins.push(
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.optimize.UglifyJsPlugin({
+    new webpack.HotModuleReplacementPlugin(),//启动module的热替换功能
+    new webpack.optimize.UglifyJsPlugin({//压缩JS文件
         compress: {
             warnings: false
         }
@@ -34,13 +34,13 @@ plugins.push(
             );
         });
     },
-    new webpack.optimize.DedupePlugin(),
-    new webpack.DefinePlugin({
+    new webpack.optimize.DedupePlugin(),//打包的时候删除重复或者相似的文件
+    new webpack.DefinePlugin({//变量定义插件，定义的变量可以在任何页面拿出来用，即使修改后，再次打开页面还是会变回我们定义的变量
         'process.env': {
             NODE_ENV: JSON.stringify('production')
         }
     }),
-    new webpack.NoErrorsPlugin()
+    new webpack.NoErrorsPlugin()//打包的时候强制通过
 );
 
 function getPagesNames(dirPath) {
